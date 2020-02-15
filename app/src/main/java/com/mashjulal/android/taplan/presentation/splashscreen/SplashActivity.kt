dad9e8a1ab86447a65db5d23c9b3600d536a52f9
mashjulal <julymashinina@gmail.com>
2020-02-15 16:48:32 +0300

    Added splash screen

---
+++ app/src/main/java/com/mashjulal/android/taplan/presentation/splashscreen/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.mashjulal.android.taplan.presentation.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mashjulal.android.taplan.R
+import com.mashjulal.android.taplan.presentation.dailyschedule.DailyScheduleActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            delay(2000L)
+            launch(Dispatchers.Main) {
+                routeToScreen()
+            }
+        }
+    }
+
+    private fun routeToScreen() {
+        startActivity(Intent(this, DailyScheduleActivity::class.java))
+        finish()
+    }
+}
