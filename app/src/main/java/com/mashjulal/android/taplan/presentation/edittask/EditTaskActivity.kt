58a786a90c2db79f76a2d6ca9518852bc78af368
mashjulal <mashjulal@gmail.com>
2020-04-04 21:17:09 +0300

    Added validation for text fields on edit screen

--- app/src/main/java/com/mashjulal/android/taplan/presentation/edittask/EditTaskActivity.kt
+++ app/src/main/java/com/mashjulal/android/taplan/presentation/edittask/EditTaskActivity.kt
@@ -6,6 +6,9 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.MenuItem
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import com.mashjulal.android.taplan.R
@@ -25,15 +28,43 @@ class EditTaskActivity : AppCompatActivity() {
         initViewModel()
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun initViewModel() {
         viewModel.selectedStartTimeLiveData.observe(this, Observer { updateTimeField(it, true) })
         viewModel.selectedEndTimeLiveData.observe(this, Observer { updateTimeField(it, false) })
         viewModel.saveTaskCompleted.observe(this, Observer { closeActivity() })
+        viewModel.hoursPerWeekValidationErrorLiveData.observe(this, Observer { updateWeekPerDayField(it) })
+        viewModel.titleValidationErrorLiveData.observe(this, Observer { updateTitleField(it) })
+        viewModel.validationPassed.observe(this, Observer { changeSaveButtonVisibility(it) })
 
         val calendar = Calendar.getInstance()
         val selectedTimePair = calendar[Calendar.HOUR_OF_DAY] to calendar[Calendar.MINUTE]
         viewModel.selectedStartTimeLiveData.value = selectedTimePair
         viewModel.selectedEndTimeLiveData.value = selectedTimePair
+        viewModel.validationPassed.value = false
+    }
+
+    private fun changeSaveButtonVisibility(visible: Boolean) {
+        if (visible) {
+            fab_save_task.show()
+        } else {
+            fab_save_task.hide()
+        }
+    }
+
+    private fun updateWeekPerDayField(error: String?) {
+        til_hours_per_week.error = error
+    }
+
+    private fun updateTitleField(error: String?) {
+        til_task_title.error = error
     }
 
     private fun closeActivity() {
@@ -45,6 +76,11 @@ class EditTaskActivity : AppCompatActivity() {
         section_start_time.setOnClickListener { showTimePickerDialog(viewModel.selectedStartTimeLiveData) }
         section_end_time.setOnClickListener { showTimePickerDialog(viewModel.selectedEndTimeLiveData) }
         fab_save_task.setOnClickListener { viewModel.saveTask() }
+        til_hours_per_week.editText?.addTextChangedListener(ValidationTextWatcher { viewModel.validateHourCount(it) })
+        til_task_title.editText?.addTextChangedListener(ValidationTextWatcher { viewModel.validateTitle(it) })
+
+        toolbar.title = ""
+        setSupportActionBar(toolbar)
     }
 
     private fun updateTimeField(timePair: Pair<Int, Int>, isStartTimePair: Boolean) {
@@ -73,4 +109,21 @@ class EditTaskActivity : AppCompatActivity() {
 
         fun newIntent(context: Context) = Intent(context, EditTaskActivity::class.java)
     }
+}
+
+private class ValidationTextWatcher(
+    private val validateValueFunction: (String) -> Unit
+): TextWatcher {
+    override fun afterTextChanged(s: Editable?) {
+        validateValueFunction.invoke(s.toString())
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+    }
+
 }
--- app/src/main/java/com/mashjulal/android/taplan/presentation/edittask/EditTaskViewModel.kt
+++ app/src/main/java/com/mashjulal/android/taplan/presentation/edittask/EditTaskViewModel.kt
@@ -1,17 +1,58 @@
 package com.mashjulal.android.taplan.presentation.edittask
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mashjulal.android.taplan.presentation.utils.SingleLiveEvent
+
+private const val MAX_HOURS_PER_WEEK = 24 * 7
 
 class EditTaskViewModel: ViewModel() {
 
     val selectedStartTimeLiveData: MutableLiveData<Pair<Int, Int>> = MutableLiveData()
     val selectedEndTimeLiveData: MutableLiveData<Pair<Int, Int>> = MutableLiveData()
     val saveTaskCompleted: SingleLiveEvent<Void> = SingleLiveEvent()
+    val hoursPerWeekValidationErrorLiveData: MutableLiveData<String?> = MutableLiveData()
+    val titleValidationErrorLiveData: MutableLiveData<String?> = MutableLiveData()
+    val validationPassed: MutableLiveData<Boolean> = MutableLiveData()
+
+    private val validationFieldsLiveDataList: List<MutableLiveData<String?>> = listOf(
+        titleValidationErrorLiveData,
+        hoursPerWeekValidationErrorLiveData
+    )
+
+    init {
+        validateTitle(null)
+        validateHourCount(null)
+    }
 
     fun saveTask() {
         saveTaskCompleted.call()
+    }
+
+    fun validateTitle(title: String?) {
+        val error = when {
+            title.isNullOrEmpty() -> "Title can't be empty"
+            else -> null
+        }
+
+        titleValidationErrorLiveData.value = error
+        validateOtherFields()
+    }
+
+    fun validateHourCount(hours: String?) {
+        val error = when {
+            hours == null -> "Hour count must be filled"
+            hours.toIntOrNull() == null -> "Field must contain only numeric characters"
+            hours.toInt() == 0 -> "Hour count can't be equal to 0"
+            hours.toInt() > MAX_HOURS_PER_WEEK -> "Hour count can't be bigger that total hours in week"
+            else -> null
+        }
+
+        hoursPerWeekValidationErrorLiveData.value = error
+        validateOtherFields()
+    }
+
+    private fun validateOtherFields() {
+        validationPassed.value = validationFieldsLiveDataList.all { it.value.isNullOrEmpty() }
     }
 }
